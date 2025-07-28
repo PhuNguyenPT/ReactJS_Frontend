1e40c28860354836f45f313056049acbf71dbba3
Khang Vy <nguyennhakvy@gmail.com>
2025-07-28 23:25:20 +0700

    Resolve merge conflict in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
-import App from "./App.tsx";
-import Signup from "./pages/Signup.tsx";
-import "./assets/fonts.css";
-
 const rootElement = document.getElementById("root");
+import "./styles/index.css";
+import "./styles/App.css";
+import App from "./App";
+import Signup from "./components/pages/SignupPage/SignupPage";
+import Login from "./components/pages/LoginPage/LoginPage";
+import LandingPage from "./components/pages/LandingPage/LandingPage";
+import "./assets/fonts.css"; 
 
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route element={<App />}>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </StrictMode>,
