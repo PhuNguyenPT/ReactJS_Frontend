4628ee15a93d2ed1ef3933165ccdb58a59c4812b
phunpt <phu.npt01@gmail.com>
2025-08-08 15:28:15 +0700

    fix scripts for pull

--- scripts/docker-pull.sh
+++ scripts/docker-pull.sh
@@ -5,14 +5,14 @@ echo "Pulling latest image..."
 sudo docker pull phunpt01/nodejs:frontend-latest
 
 echo "Stopping and removing container..."
-sudo docker-compose stop frontend
-sudo docker-compose rm -f frontend
+sudo docker compose stop frontend
+sudo docker compose rm -f frontend
 
 echo "Starting container with new image..."
-sudo docker-compose up -d frontend
+sudo docker compose up -d frontend
 sudo docker image prune -f
 
 echo "Done! Container updated successfully."
 
 # Show status
-sudo docker-compose ps frontend
+sudo docker compose ps frontend
