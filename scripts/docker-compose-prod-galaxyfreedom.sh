75ade446dfc57eed79ffcd13c66e47311c248bb6
phunpt <phu.npt01@gmail.com>
2025-12-15 23:07:31 +0700

    remove unused docker compose and scripts

--- scripts/docker-compose-prod-galaxyfreedom.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker compose \
-  --env-file .env.production \
-  -f docker-compose.prod.galaxyfreedom.com.yml \
-  -p frontend-prod-galaxyfreedom \
-  up -d --build
--- scripts/docker-compose-staging-galaxyfreedom.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker compose \
-  --env-file .env.staging \
-  -f docker-compose.staging.galaxyfreedom.com.yml \
-  -p frontend-staging-galaxyfreedom \
-  up -d --build
