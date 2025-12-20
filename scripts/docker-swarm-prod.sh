89eabef0d40b1de98d5de5f99ff10b6deb58f18a
phunpt <phu.npt01@gmail.com>
2025-12-20 19:11:26 +0700

    add swarm prod, change host to ingress in staging

---
+++ scripts/docker-swarm-prod.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if ! docker network ls --format '{{.Name}}' | grep -q '^frontend-swarm-network-prod$'; then
+    docker network create --driver overlay --attachable frontend-swarm-network-prod
+fi
+set -a
+source .env.prod
+set +a
+
+docker stack deploy -c docker-compose.swarm.prod.yml frontend-swarm-prod --detach=false
